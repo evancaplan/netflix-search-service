ab65753d1ca3b6abf8a2bf8ad3b2178fc81b9a0a
evancaplan <ecaplan31@gmail.com>
2023-03-22 13:40:38 -0400

    Update For CORS and ACCESS Token

--- findCountries.py
+++ findCountries.py
@@ -14,5 +14,10 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Methods': 'GET,OPTIONS',
+        },
         'body': json.dumps(countries)
     }
